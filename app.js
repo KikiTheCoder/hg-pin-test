bafe3fcacd2a4ad39d8fbb33fb5d544a4ba47a06
djc-arduino <82812509+djc-arduino@users.noreply.github.com>
2021-10-16 15:01:33 +0200

    Update app.js

--- app.js
+++ app.js
@@ -7,28 +7,9 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
-app.get("/pin/:pin/:state", (req, res) => {
-    if (isNaN(req.params.pin)) {
-        res.send("Pin Not a Number").status(400)
-        return
-    }
-    if (isNaN(req.params.state)) {
-        res.send("State Not a Number").status(400)
-        return
-    }
-
-    let lednr = parseInt(req.params.pin)
-    let led = new Gpio(lednr, "out")
-    let state = parseInt(req.params.state)
-    
-    console.log(`Pin: ${lednr}\t|\tState: ${state}`);
-
-    led.writeSync(state);
-
-    res.send(`Switched ${req.params.pin}`)
-})
 
 const switchIn = new Gpio( '16', 'in', 'both' );
+
 
 switchIn.watch( ( err, value ) => {
   if( err ) {
@@ -38,7 +19,6 @@ switchIn.watch( ( err, value ) => {
   // log pin value (0 or 1)
   console.log( 'Pin value', value );
 } );
-
 
 
 app.listen(port, () => {
